4f0afafd2357a324fcb6ee37947f717bf0e1e7f2
oksipo <makoveychukvitaliy@gmail.com>
2017-05-24 02:19:24 +0300

    Login

--- AspNetCoreSpa/Client/app/Authorization/login.component.ts
+++ AspNetCoreSpa/Client/app/Authorization/login.component.ts
@@ -1,10 +1,21 @@
 ﻿import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService} from '../Services/authService'
 @Component({
     selector: "login",
     templateUrl: './login.component.html',
     styleUrls: ['login.component.css']
 })
 export class LoginComponent {
-
+    constructor(private auth: AuthService, private router: Router) { }
+    email: string;
+    password: string;
+    login = (() => {
+        this.auth.login(
+            {
+                email: this.email,
+                password: this.password,
+                rememberMe:true
+            }).subscribe(x => this.router.navigateByUrl("home"));
+    })
 }
