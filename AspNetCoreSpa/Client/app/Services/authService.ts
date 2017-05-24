d11ce3b5d5c7d1d4a72ba3bb57ac3dc75c986288
oksipo <makoveychukvitaliy@gmail.com>
2017-05-24 22:51:25 +0300

    Revert "Login"
    This reverts commit 4f0afafd2357a324fcb6ee37947f717bf0e1e7f2.

--- AspNetCoreSpa/Client/app/Services/authService.ts
+++ AspNetCoreSpa/Client/app/Services/authService.ts
@@ -1,24 +1,14 @@
 ﻿import { Injectable } from '@angular/core'
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { LocalStorage, StorageProperty } from 'h5webstorage';
+import { LocalStorage } from 'h5webstorage';
 @Injectable()
 export class AuthService {
 
     constructor(private _http: Http, private localStorage: LocalStorage) { }
-    @StorageProperty() localId: string;
-
-    public isAuthorized():boolean {
-        return this.localId != "";
-    }
 
     public register(model: any): Observable<any> {
         return this._http.post("api/account/register/", model).map(res => { this.localStorage.setItem("localId", res.json().id); return res.json() })
-            .catch(err => { console.error(err); throw (err); });
-    }
-
-    public login(model: any): Observable<any> {
-        return this._http.post("api/account/login/", model).map(res => { this.localStorage.setItem("localId", res.json().id); return res.json() })
             .catch(err => { console.error(err); throw (err); });
     }
 }
