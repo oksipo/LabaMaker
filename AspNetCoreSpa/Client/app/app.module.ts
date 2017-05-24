5e56eccae40b0c6d4205a032d0539fea85fe9243
AnastasiyaPolianska <anastasiya_polianska@mail.ru>
2017-05-24 17:23:28 +0300

    help, questions added

--- AspNetCoreSpa/Client/app/app.module.ts
+++ AspNetCoreSpa/Client/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
+import { ButtonModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
@@ -12,6 +14,7 @@ import { HomeComponent } from './home/home.component'
 import { WorksComponent } from './allWorks/works.component'
 import { AboutUsComponent } from './aboutUs/aboutUs.component'
 import { HelpComponent } from './help/help.component'
+import { QuestionsComponent } from './questions/questions.component'
 import { MySharedModule } from './shared/shared.module';
 import { TestComponent } from "./Test/test"
 import { LoginComponent } from "./Authorization/login.component"
@@ -22,6 +25,7 @@ const AppRoutes: Routes = [
     { path: 'allWorks', component: WorksComponent },
     { path: 'aboutUs', component: AboutUsComponent },
     { path: 'help', component: HelpComponent },
+    { path: 'questions', component: QuestionsComponent },
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: "test", component: TestComponent },
@@ -29,14 +33,16 @@ const AppRoutes: Routes = [
 ];
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, WorksComponent, TestComponent, LoginComponent, RegisterComponent, AboutUsComponent, HelpComponent],
+    declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, WorksComponent, TestComponent, LoginComponent, RegisterComponent, AboutUsComponent, HelpComponent, QuestionsComponent],
     imports: [
         BrowserAnimationsModule,
         BrowserModule,
         // FormsModule,
         HttpModule,
         MySharedModule,
-        RouterModule.forRoot(AppRoutes)],
+        RouterModule.forRoot(AppRoutes),
+        Ng2PageScrollModule.forRoot(),
+        ButtonModule],
     providers: [{ provide: APP_BASE_HREF, useValue: '/' }
     ],
     bootstrap: [AppComponent]
